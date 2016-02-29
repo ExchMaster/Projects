b0ac56ad7579ac33a72492e0be3c5313e4026dcc
ExchMaster <jingram@microsoft.com>
2016-02-28 20:03:36 -0500

    Update ConfigStore

--- GadgetronV2/GadgetronV2/Templates/configStor.sh
+++ GadgetronV2/GadgetronV2/Templates/configStor.sh
@@ -3,15 +3,15 @@
 apt-get -qq install cifs-utils -y
 
 #Create directory, udpate fstab, mount Azure file share
-mkdir /mnt/$2
-echo "//$1.file.core.windows.net/$2 /mnt/$2 cifs vers=3.0,username=$1,password=$3,dir_mode=0777,file_mode=0777" >> /etc/fstab
+mkdir /AzFiles/$2
+echo "//$1.file.core.windows.net/$2 /AzFiles/$2 cifs vers=3.0,username=$1,password=$3,dir_mode=0777,file_mode=0777" >> /etc/fstab
 mount -a
 
 #Download script, update crontab, copy files as needed to get VMSS Instance ID
 wget https://raw.githubusercontent.com/ExchMaster/Projects/master/GadgetronV2/GadgetronV2/Templates/instID.sh -P /bin -N -q
 chmod +x /bin/instID.sh
 /bin/instID.sh
-cp /10.0.247.* /mnt/$2
+cp /10.0.247.* /AzFiles/$2
 echo "@reboot root /bin/instID.sh" >> /etc/crontab
 echo "* * * * * root /bin/instID.sh" >> /etc/crontab
-echo "* * * * * root cp /10.0.247.* /mnt/$2" >> /etc/crontab
+echo "* * * * * root cp /10.0.247.* /AzFiles/$2" >> /etc/crontab
